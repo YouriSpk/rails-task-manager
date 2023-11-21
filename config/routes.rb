899988790ca017fa17c0fb65427d3072234fe47c
Youri Soposki <youri.soposki@gmail.com>
2023-11-21 21:55:04 +0100

    first draft styling

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'tasks', to: "tasks#index", as: :tasks
-  get 'tasks/:id', to: "tasks#show", as: :task_id
+  get 'tasks/:id', to: "tasks#show", as: :task, constraints: { id: /\d+/ }
+  get 'tasks/new', to: "tasks#new", as: :new_task
+  post 'tasks', to: "tasks#create", as: :create_task
+  get 'tasks/:id/edit', to: "tasks#edit", as: :edit_task
+  patch 'tasks/:id', to: "tasks#update", as: :update_task
+  delete 'tasks/:id', to: "tasks#destroy", as: :destroy_task
 end
